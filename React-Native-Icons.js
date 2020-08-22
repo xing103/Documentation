6c423563b11e739da3383cdadfc2b19d04c0f8e0
xing103 <59462765+xing103@users.noreply.github.com>
2020-08-22 00:08:41 -0400

    啊

---
+++ React-Native-Icons.js
@@ -0,0 +1,15 @@
+/* 
+    Docs:
+        学习的最佳方式：把重要的知识点归纳并集中在一起，集中复习，省去大量时间去查找相关资料
+        web-icons: https://oblador.github.io/react-native-vector-icons/
+        web-docs: https://docs.expo.io/guides/icons/
+
+    Steps:
+        1. import { Ionicons } from '@expo/vector-icons'; 
+        // Ionicons是变量，根据引用公司的名称而改变
+        // 例：使用AntDesign时候要替换Ionicons成AntDesign
+
+        2. <AntDesign name="stepforward" size={24} color="black" />
+        //现在使用的是AntDesign的图片和属性
+
+*/
